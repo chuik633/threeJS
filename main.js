55d34c14c819ba06ca446fe4bcde757d7adb6cfe
Katherine Chui <chuik633@newschool.edu>
2025-11-17 20:33:47 -0800

    movement

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
 const colors = {
   blue: "#6371AD",
   red: "#E53C29",
@@ -31,6 +33,7 @@ scene.add(plane);
 
 // const material = new THREE.MeshBasicMaterial({ color: colors.yellow });
 // const cube = new THREE.Mesh(geometry, material);
+let objects = [];
 const r = planeW / 10;
 const c = planeH / 10;
 for (let i = 0; i < 10; i++) {
@@ -49,6 +52,11 @@ for (let i = 0; i < 10; i++) {
     const material = new THREE.MeshBasicMaterial({ color: colors.yellow });
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+    objects.push({
+      object: cube,
+      direction: "y",
+      speed: Math.random(),
+    });
 
     // yellow part
     const w2 = Math.random() * 0.3;
@@ -59,11 +67,36 @@ for (let i = 0; i < 10; i++) {
     geometry2.translate(
       i * r - planeW / 2,
       c * j - planeH / 2,
-      Math.random() * 5
+      Math.random() * 4
     );
     const material2 = new THREE.MeshBasicMaterial({ color: colors.red });
     const cube2 = new THREE.Mesh(geometry2, material2);
     scene.add(cube2);
+    objects.push({
+      object: cube2,
+      direction: "z",
+      speed: Math.random(),
+    });
+
+    // yellow part
+    const w3 = Math.random() * 0.1;
+    const geometry3 = new THREE.TetrahedronGeometry(
+      w3,
+      Math.floor(Math.random() * 2)
+    );
+    geometry3.translate(
+      i * r - planeW / 2,
+      c * j - planeH / 2,
+      Math.random() * 3
+    );
+    const material3 = new THREE.MeshBasicMaterial({ color: "#F1EDDE" });
+    const cube3 = new THREE.Mesh(geometry3, material3);
+    scene.add(cube3);
+    objects.push({
+      object: cube3,
+      direction: "x",
+      speed: Math.random(),
+    });
   }
 }
 
@@ -72,7 +105,25 @@ camera.position.y = 15;
 camera.position.x = 4;
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
-
+let time = 0;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 function animate() {
+  //called every 60 seconds
+  time += 0.01;
+  controls.update();
+  for (const objectInfo of objects) {
+    console.log(objectInfo);
+    if (objectInfo.direction == "y") {
+      objectInfo.object.rotation.y = Math.sin(time * objectInfo.speed);
+    } else if (objectInfo.direction == "z") {
+      objectInfo.object.rotation.z = Math.sin(time * objectInfo.speed);
+    } else {
+      objectInfo.object.rotation.x = Math.sin(time * objectInfo.speed);
+    }
+
+    objectInfo.object.y += Math.sin(time * objectInfo.speed) * 10;
+  }
+  // change positions etc so that u can animate here
   renderer.render(scene, camera);
 }
